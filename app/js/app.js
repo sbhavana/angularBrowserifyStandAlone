a9fb59126275dd4787ed83e4208db0a91f475e8e
Bhavana <bhavana.shah@gsynergy.com>
2013-11-27 20:02:19 +0530

    Added socket-io based creation/updation of users

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,11 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'btford.socket-io'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/list', {templateUrl: 'partials/list.html', controller: 'ListController'});
+  $routeProvider.when('/new', {templateUrl: 'partials/detail.html', controller: 'CreateController'});
+  $routeProvider.when('/edit/:userId', {templateUrl: 'partials/detail.html', controller: 'EditController'});
+  $routeProvider.otherwise({redirectTo: '/list'});
 }]);
