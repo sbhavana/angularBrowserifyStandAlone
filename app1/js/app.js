9830946aeec28eb352b9303ebff46757ff89196a
Bhavana <bhavana.shah@gsynergy.com>
2013-12-02 12:08:08 +0530

    Added new server and client folder. The main app's controller, services and configuration reside in a single module now. Also, modified socket.js service code to make callback work for emit.

---
+++ app1/js/app.js
@@ -0,0 +1,162 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+        'ngRoute',
+        'btford.socket-io'
+    ])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/list', {templateUrl: 'templates/list.html', controller: 'ListController'});
+        $routeProvider.when('/new', {templateUrl: 'templates/detail.html', controller: 'CreateController'});
+        $routeProvider.when('/edit/:userId', {templateUrl: 'templates/detail.html', controller: 'EditController'});
+        $routeProvider.otherwise({redirectTo: '/list'});
+    }])
+
+    .value('version', '0.1')
+
+// create and register a uuid service
+
+    .service ( 'uuid', [ function () {
+
+    return {
+
+        newUUID: function () {
+
+            function _p8(s) {
+                var p = (Math.random().toString(16)+"000000000").substr(2,8);
+                return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+            }
+
+            return _p8() + _p8(true) + _p8(true) + _p8();
+        }
+    }
+}])
+
+    .controller( 'ListController', [ '$scope', 'socket', function ( $scope, socket ) {
+
+        // get users list from the server
+        socket.emit ( 'getAllUsers', function ( err, data ) {
+
+            if ( err ) {
+
+                console.log ( err );
+            }
+
+            else {
+
+                console.log ( "getAllUsers callback: ", data );
+
+                $scope.users = data;
+            }
+        });
+
+        socket.on ( 'userDeleted', function ( data ) {
+
+            console.log ( "userDeleted: ", data );
+
+            // update $scope.users
+            $scope.users = window._.filter ( $scope.users, function ( user ) {
+
+                return user._id !== data._id;
+            });
+        });
+
+        socket.on ( 'newUserAdded', function ( data ) {
+
+            console.log ( "newUserAdded: ", data );
+
+            // update $scope.users
+            $scope.users.push ( data );
+        });
+
+        socket.on ( 'userUpdated', function ( data ) {
+
+            console.log ( "userUpdated: ", data );
+
+            // update $scope.users
+            var ix = window._.findIndex ( $scope.users, function ( user ) {
+
+                return user._id === data._id;
+            });
+
+            $scope.users [ ix ] = data;
+        });
+    }])
+
+    .controller( 'CreateController', [ '$scope', '$location', 'socket', 'uuid', function ( $scope, $location, socket ) {
+
+        $scope.save = function () {
+
+            // send the new user details to the server to add
+            socket.emit ( 'addNewUser', $scope.user,  function ( err ) {
+
+                if ( err ) {
+
+                    console.log ( err );
+                }
+
+                else {
+
+                    console.log ( "addNewUser callback" );
+                    $location.path('/');
+                }
+            });
+        };
+    }])
+
+    .controller( 'EditController', [ '$scope', '$location', '$routeParams', 'socket', function ( $scope, $location, $routeParams, socket ) {
+
+        var state = "Editing";
+
+        // get user from the server
+        socket.emit ( 'getUser', { _id: $routeParams.userId },  function ( err, data ) {
+
+            if ( err ) {
+
+                console.log ( err );
+            }
+
+            else {
+
+                console.log ( "getUser callback: ", data );
+                $scope.user = data;
+            }
+        });
+
+        $scope.save = function () {
+
+            // send the updated user details to the server to add
+            socket.emit ( 'updateUser', $scope.user,  function ( err ) {
+
+                if ( err ) {
+
+                    console.log ( err );
+                }
+
+                else {
+
+                    console.log ( "updateUser callback" );
+                    $location.path('/');
+                }
+            });
+        };
+
+        $scope.destroy = function () {
+
+            // send the user details to the server to delete
+            socket.emit ( 'deleteUser', $scope.user, function ( err ) {
+
+                if ( err ) {
+
+                    console.log ( err );
+                }
+
+                else {
+
+                    console.log ( "deleteUser callback" );
+                    $location.path('/');
+                }
+            });
+        };
+    }]);
+
